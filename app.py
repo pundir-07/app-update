1d9fb190ffd843e30d3d4b54d451ea0fe4e820f7
vp <vaibhavpundir07@gmail.com>
2025-07-08 06:40:17 +0530

    first update release done

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # app.py
 import tkinter as tk
-
+from updater import main as check_for_updates
 def main():
     root = tk.Tk()
     root.title("My Python App Version Updated")
     root.geometry("300x150")
-
+    check_for_updates()
     label = tk.Label(root, text="Hello, this is my app!")
     label.pack(pady=20)
 
